9e5a5fb45fd6b1223951fc5d641e286539855458
Saveliy <sava2698@gmail.com>
2023-05-25 14:27:15 +0600

    Comments modified

--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -3,24 +3,51 @@ import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
 import { CommentEntity } from './entities/comment.entity';
 import { AppDataSource as appDataSource } from 'src/data-source';
+import { UserEntity } from 'src/user/entities/user.entity';
+import { User } from 'src/decorators/user.decorator';
 
 @Injectable()
 export class CommentService {
   private readonly commentsRepository =
     appDataSource.getRepository(CommentEntity);
 
-  create(dto: CreateCommentDto) {
-    return this.commentsRepository.save({
+  async create(dto: CreateCommentDto, userId: number) {
+    const comment = await this.commentsRepository.save({
       text: dto.text,
       post: {
         id: dto.postId,
       },
-      user: { id: 11 },
+      user: { id: userId },
+    });
+
+    return this.commentsRepository.findOne({
+      where: {
+        id: comment.id,
+      },
+      relations: ['user'],
     });
   }
 
-  findAll() {
-    return this.commentsRepository.find();
+  async findAll(postId: number) {
+    const qb = this.commentsRepository.createQueryBuilder('c');
+
+    if (postId) {
+      qb.where('c.postId = :postId', { postId });
+    }
+
+    const arr = await qb
+      .leftJoinAndSelect('c.post', 'post')
+      .leftJoinAndSelect('c.user', 'user')
+      .getMany();
+
+    return arr.map((obj) => {
+      return {
+        ...obj,
+        post: {
+          id: obj.post.id,
+        },
+      };
+    });
   }
 
   findOne(id: number) {
